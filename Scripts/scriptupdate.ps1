40e58dd4240f00ce89dc5a6a79afd60b97737d3b
mspriya92 <157378385+mspriya92@users.noreply.github.com>
2024-07-26 17:10:11 +0530

    Update scriptupdate.ps1

--- Scripts/scriptupdate.ps1
+++ Scripts/scriptupdate.ps1
@@ -1,16 +1,22 @@
-# Parameters
 param (
-    [string]$fileToEncrypt = "staging/nsi.json",
-	   [string]$encryptedFile = "staging/nsi_encrypted.json"
-    )
- 
+  [string]$fileToEncrypt = "staging/nsi.json",
+  [string]$encryptedFile = "staging/nsi_encrypted.json"
+)
+
 # Update nsi.json with secrets
 echo "Updating nsi.json with secrets"
 $json = Get-Content -Path $fileToEncrypt -Raw | ConvertFrom-Json
+
+# Update specific properties with secrets from environment variables
 $json.Sphere.DockerUsername = "$env:DOCKER_USERNAME"
 $json.Sphere.DockerPassword = "$env:DOCKER_PASSWORD"
 $json.Sphere.APIkey = "$env:prod_API_KEY"
-$json | ConvertTo-Json | Set-Content -Path $fileToEncrypt
+
+# Convert the updated JSON object back to a string
+$updatedJsonString = $json | ConvertTo-Json
+
+# Write the updated JSON string to the file (no need for encryption here)
+Set-Content -Path $fileToEncrypt -Value $updatedJsonString
 
 # Generate a secure key (replace 'MySuperSecretKey!' with a valid 256-bit key in Base64 encoding)
 $keyBase64 = "EtQ8V5KHb5d//DsjSklYVIJi8xmfDOYNwKHmgVI2Ix3JahiQNJZZRLO1jWOuwDwa"  # Example key, replace with your own
