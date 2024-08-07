9029e78c439938478b44344749f9b1907b9b23ec
mspriya92 <157378385+mspriya92@users.noreply.github.com>
2024-08-07 15:43:03 +0530

    Update Devcode.ps1

--- .github/Test/Devcode.ps1
+++ .github/Test/Devcode.ps1
@@ -20,7 +20,7 @@ $json.Sphere.DockerPassword = "$env:DOCKER_PASSWORD"
 $json.Sphere.APIkey = "$env:prod_API_KEY"
 #$json.Sphere.APIvalue = "$env:prod_API_VALUE"
 #$json.Sphere.APIconfig = "$env:prod_APICONFIG"
-$json.Sphere.AppConfigEndpoint = $AppConfigUrl
+$json.Sphere.AppConfigEndpoint = "$AppConfigUrl"
 $json | ConvertTo-Json | Set-Content -Path $fileToEncrypt
 
 # Generate a secure key (replace 'MySuperSecretKey!' with a valid 256-bit key in Base64 encoding)
